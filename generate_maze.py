bdf19324f8aa307a7f055dc82d80c21f352a26b2
ahdediu <adediu@autonoma.pt>
2024-05-21 22:15:54 +0100

    21 May 2023

--- generate_maze.py
+++ generate_maze.py
@@ -1,18 +1,21 @@
 import collections
-import itertools
 import random
 from symtable import Symbol
 from typing import Set, Tuple, List, FrozenSet, Optional
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+from networkx import is_connected
 from pyformlang.finite_automaton import DeterministicFiniteAutomaton, State
 import os
+import networkx as nx
+
+
 class Maze:
-    def __init__(self, m: int, n: int,start:Tuple=(0,0), end: tuple = None) -> None:
-        self.m = m
-        self.n = n
-        self.start = start
-        self.end = end if end is not None else (m - 1, n - 1)
+    def __init__(self, m: int, n: int, start: Tuple = (0, 0), end: tuple = None) -> None:
+        self.m: int = m
+        self.n: int = n
+        self.start: int = start
+        self.end: int = end if end is not None else (m - 1, n - 1)
         self.vertices = set()
         self.edges = set()
         self.unvisited_vertices = {(i, j) for i in range(m) for j in range(n)}
@@ -20,72 +23,96 @@ class Maze:
         self.random_walk_until_meets_maze(meet_itself=True)
         while len(self.vertices) < self.m * self.n:
             self.random_walk_until_meets_maze(meet_itself=False)
+
+        self.graph = nx.Graph()
+        self.graph.add_nodes_from(self.vertices)
+        self.graph.add_edges_from(self.edges)
+
         self.remove_extra_edges()
-    def remove_extra_edges(self):
+        #peek = iter(self.edges) then next(peek) #
+        self.solution_path: Set[FrozenSet[Tuple[int, int]]] =  self.find_path()
 
-        # function to find a path, say depth-first search
-        # from top left to bottom right of the maze
+        """
+        remove extra edges is tricky
+        When find_path is called, it determines a temporary path from the start to the current vertex. 
+        But only removing an edge not on this temporary path doesn't guarantee that 
+        the vertex is still connected to the start point. 
+        The edge you're removing might be part of a different path that's 
+        currently the only way to reach another vertex.
+        """
 
-        # get the path
-        path = self.find_path()
 
-        # iterate through all vertices, if its not in path
-        # and it has more than 3 edges, remove one at random
+
+    def is_connected(self) -> bool:
+        return nx.is_connected(self.graph)
+    def remove_extra_edges(self) -> None:
         for vertex in self.vertices:
-            if vertex not in path:
-                edges = [e for e in self.edges if vertex in e]
-                if len(edges) > 3:
-                    self.edges.remove(random.choice(edges))
-
-        # for vertices on the path with more than 3 edges
-        # remove an edge which is not on the path.
-        for vertex in path:
-            edges = [e for e in self.edges if vertex in e]
-            if len(edges) > 3:
-                for edge in edges:
-                    if not any(e in path for e in edge):
-                        self.edges.remove(edge)
+            vertex_edges = [e for e in self.edges if vertex in e]
+            if len(vertex_edges) > 3:
+                for edge_to_try in vertex_edges:
+                    # Temporarily remove the edge from both self.edges and self.graph
+                    self.edges.remove(edge_to_try)
+                    self.graph.remove_edge(*edge_to_try)
+                    if not self.is_connected():
+                        self.edges.add(edge_to_try)
+                        self.graph.add_edge(*edge_to_try)
+                    else:
                         break
 
-        return
+    # function to find a path, say depth-first search
+# from top left to bottom right of the maze
 
-    def adjacent_vertices(self, vertex):
-            return [v for edge in self.edges for v in edge
-                    if v != vertex and edge not in self.edges_of_vertex(vertex)]
-
-    def find_path(self) -> Optional[List[Tuple[int, int]]]:
-            """
-            Perform a breadth-first search to find a path from start to end.
-
-            :return: List of tuples representing the path from start_point to end_point, or
-                     None if no such path exists.
-            """
-
-            visited = set()  # A set to store visited vertices.
-            queue = collections.deque([[self.start]])  # Initialize the queue with the start point.
-
-            while queue:  # Continue until all paths are exhausted.
-                path = queue.popleft()  # Pop the next path from the queue.
-                vertex = path[-1]  # Get the last vertex from the path.
-
-                # If this vertex is the end point, we've found a path!
-                if vertex == self.end:
-                    return path
-
-                elif vertex not in visited:  # If the vertex has not been visited yet,
-                    for adjacent in self.adjacent_vertices(vertex):  # iterate over adjacent vertices
-                        new_path = list(path)  # Create a new path from the old one
-                        new_path.append(adjacent)  # Add the adjacent vertex to the new path
-                        queue.append(new_path)  # Add the new path to the queue
-
-                    visited.add(vertex)  # Mark the current vertex as visited
-
-            return None  # No path was found
+# get the path
+#     def find_path(self, end_path: Optional[Tuple[int, int]] = None) -> Optional[Set[FrozenSet[Tuple[int, int]]]]:
+#         """
+#         Perform a breadth-first search to find a path from start to end.
+#
+#         :param end_path: The end point for the path calculation.
+#         :return: Set of frozensets. Each frozenset represents an edge,
+#                  expressed as a pair of vertices (where each vertex is a tuple of coordinates).
+#                  None if no such path exists.
+#         """
+#         if end_path is None:
+#             end_path = self.end
+#
+#         visited = set()  # A set to store visited vertices.
+#         queue = collections.deque([[self.start]])  # Initialize the queue with the start point.
+#
+#         while queue:  # Continue until all paths are exhausted.
+#             path = queue.popleft()  # Pop the next path from the queue.
+#             vertex = path[-1]  # Get the last vertex from the path.
+#
+#             if vertex in visited:  # If the vertex has been visited, skip to next iteration
+#                 continue
+#
+#             visited.add(vertex)  # Mark the current vertex as visited
+#
+#             for adjacent in self.adjacent_vertices(vertex):  # iterate over adjacent vertices
+#                 new_path = list(path)  # Create a new path from the old one
+#                 new_path.append(adjacent)  # Add the adjacent vertex to the new path
+#
+#                 if adjacent == end_path:  # Found a path
+#                     return {frozenset({new_path[i - 1], new_path[i]}) for i in range(1, len(new_path))}
+#
+#                 queue.append(new_path)  # Add the new path to the queue
+#
+#         return None  # No path was found
+    def find_path(self) -> Set[FrozenSet[tuple]]:
+        try:
+            # Use NetworkX's shortest path function
+            path = nx.shortest_path(self.graph, self.start, self.end)
+            # Convert the returned node path into edge path, and use frozenset for edges
+            edge_path = {frozenset([path[i - 1], path[i]]) for i in range(1, len(path))}
+            return edge_path
+        except nx.NetworkXNoPath:
+            print("No path between start and end.")
+            return set()
 
     def add(self, vertices: List[Tuple[int, int]], edges: Set[FrozenSet[Tuple[int, int]]]) -> None:
         self.vertices.update(vertices)
         self.edges.update(edges)
         self.unvisited_vertices.difference_update(vertices)
+
 
     def edges_of_vertex(self, vertex: Tuple[int, int]) -> Set[Set[Tuple[int, int]]]:
         """Returns a set of valid edges for a given vertex within the grid."""
@@ -102,7 +129,7 @@ class Maze:
         return edges
 
 
-    def random_walk_until_meets_maze(self, meet_itself:bool)-> None:
+    def random_walk_until_meets_maze(self, meet_itself: bool) -> None:
         # collateral effect, updates the edges and the vertices of the maze itself.
         if not self.unvisited_vertices:
             # Handle if there are no unvisited vertices, e.g. return, or raise an exception.
@@ -134,6 +161,40 @@ class Maze:
         return
 
 
+    def draw_maze(self) -> None:
+        """
+          This method visualizes the maze.
+          It uses Matplotlib to draw the maze's paths.
+          Red lines indicate the solution of the maze.
+          The maze is drawn in a separate pop-up window.
+          """
+        fig, ax = plt.subplots()
+
+        ax.axis('off')  # turning off the axis
+        # getting x and y coordinates of all points in the maze
+
+        x_values, y_values = zip(*[point for edge in self.edges for point in edge])
+        ax.set_xlim(min(x_values) - 1, max(x_values) + 1)
+        ax.set_ylim(min(y_values) - 1, max(y_values) + 1)
+
+        ax.xaxis.set_major_locator(ticker.IndexLocator(base=1, offset=0))
+        ax.yaxis.set_major_locator(ticker.IndexLocator(base=1, offset=0))
+
+        ax.grid(which='major', color='k')
+        # drawing the edges of the maze
+        for edge in self.edges:
+            x1, y1 = list(edge)[0]
+            x2, y2 = list(edge)[1]
+            if edge in self.solution_path:
+                ax.plot([x1, x2], [y1, y2], color='r', linewidth=2)
+            else:
+                ax.plot([x1, x2], [y1, y2], color='k', linewidth=1)
+        # space for lables
+        # for (x1, y1), (x2, y2) in maze:
+        #     ax.text(x1, y1, str(x1 + y1*3), color="blue", fontsize=9,ha='center', va='center' )
+        #     ax.text(x2, y2, str(x2  + y2*3), color="blue", fontsize=9,ha='center', va='center' )
+        plt.show()  # display the plot
+
 
 def create_GAP_transition_table(maze: Set[Tuple[int, int]], m: int, n: int) -> list:
     transition_table = [[i for i in range(m * n)] for _ in range(4)]  # Default transition is self
@@ -144,8 +205,8 @@ def create_GAP_transition_table(maze: Set[Tuple[int, int]], m: int, n: int) -> l
         vx, vy = v
 
         # Calculate state numbers
-        state_u = ux  + uy*m
-        state_v = vx  + vy*m
+        state_u = ux + uy * m
+        state_v = vx + vy * m
 
         if ux == vx:  # the edge is vertical    uy
             if uy > vy:  # the edge goes N-S    vy
@@ -165,31 +226,8 @@ def create_GAP_transition_table(maze: Set[Tuple[int, int]], m: int, n: int) -> l
     return transition_table
 
 
-
-
-def draw_maze(maze):
-    fig, ax = plt.subplots()
-
-    ax.axis('off')
-
-    x_values, y_values = zip(*[point for edge in maze for point in edge])
-    ax.set_xlim(min(x_values) - 1, max(x_values) + 1)
-    ax.set_ylim(min(y_values) - 1, max(y_values) + 1)
-
-    ax.xaxis.set_major_locator(ticker.IndexLocator(base=1, offset=0))
-    ax.yaxis.set_major_locator(ticker.IndexLocator(base=1, offset=0))
-
-    ax.grid(which='major', color='k')
-
-    for (x1, y1), (x2, y2) in maze:
-        ax.plot([x1, x2], [y1, y2], color='r', linewidth=1)
-
-    # for (x1, y1), (x2, y2) in maze:
-    #     ax.text(x1, y1, str(x1 + y1*3), color="blue", fontsize=9,ha='center', va='center' )
-    #     ax.text(x2, y2, str(x2  + y2*3), color="blue", fontsize=9,ha='center', va='center' )
-    plt.show()
-def create_dfa(maze:Maze) -> DeterministicFiniteAutomaton:
-    transition_table: list=create_GAP_transition_table(maze.edges, maze.m, maze.n)
+def create_dfa(maze: Maze) -> DeterministicFiniteAutomaton:
+    transition_table: list = create_GAP_transition_table(maze.edges, maze.m, maze.n)
     dfa = DeterministicFiniteAutomaton()
 
     direction_to_symbol = {
@@ -221,30 +259,29 @@ def create_dfa(maze:Maze) -> DeterministicFiniteAutomaton:
     dfa.add_final_state(State(maze.end))
     return dfa
 
+
 def dfa_is_minimal(dfa: DeterministicFiniteAutomaton):
     from pyformlang import finite_automaton as fa
     minimal_dfa = dfa.minimize()
     return dfa == minimal_dfa
 
 
-
-m:int=25 # horizontal grid size
-n:int=20 # vertical grid size
+m: int = 25  # horizontal grid size
+n: int = 20  # vertical grid size
 maze = Maze(m, n)
-draw_maze(maze.edges)
-#t=create_GAP_transition_table(maze.edges, m, n)
-#print(t)
+maze.draw_maze()
+# t=create_GAP_transition_table(maze.edges, m, n)
+# print(t)
 # Initialize the automaton (replace this with your current automaton)
 dfa = DeterministicFiniteAutomaton()
 # ... (define your dfa here)
-#dfa.tr
+# dfa.tr
 # Check for minimality
 is_minimal = dfa_is_minimal(dfa)
 print(f'The automaton is {"minimal" if is_minimal else "not minimal"}')
 # Save the result to a text file
 # Be sure to use the path where you want to save the results
-#with open('path/to/your/minimality_result.txt', 'w') as file:
+# with open('path/to/your/minimality_result.txt', 'w') as file:
 #    file.write(f'The automaton is {"minimal" if is_minimal else "not minimal"}.\n')
 
-#print(f'Minimality result saved to text file.')
-
+# print(f'Minimality result saved to text file.')
